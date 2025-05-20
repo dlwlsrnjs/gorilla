69542c23518c8875954a00052265542cc0508eb2
hailly0101 <79821691+hailly0101@users.noreply.github.com>
2025-05-20 07:44:06 +0000

    add-data

--- raft/meme_data/scripts/generate_meme_dataset_json.py
+++ raft/meme_data/scripts/generate_meme_dataset_json.py
@@ -4,9 +4,9 @@ import json
 import sys
 
 # ✅ 실제 경로 설정
-IMAGES_DIR = Path("/workspaces/gorilla/raft/meme_data/memes/1_earth-chan_meme_pinterest")
-DOCS_DIR = Path("/workspaces/gorilla/raft/meme_data/docs/environment")
-OUTPUT_JSON = Path("/workspaces/gorilla/raft/meme_data/scripts/earth-chan.json")
+IMAGES_DIR = Path("/workspaces/gorilla/raft/meme_data/memes/5_privilege-denying-dude_meme_pinterest")
+DOCS_DIR = Path("/workspaces/gorilla/raft/meme_data/docs/gender")
+OUTPUT_JSON = Path("/workspaces/gorilla/raft/meme_data/scripts/privilege-denying-dude.json")
 
 
 def clean_keyword(folder_name):
